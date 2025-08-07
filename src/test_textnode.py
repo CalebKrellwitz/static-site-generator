9de7e12140722e0d515bc766691d8f5aac2705dc
CalebKrellwitz <cwkrellwitz@gmail.com>
2025-08-07 10:42:36 -0400

    create split_nodes_delimiter()

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, split_nodes_delimiter, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +23,74 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.LINK, "https://github.com")
         node2 = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
         self.assertNotEqual(node, node2)
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_bold(self):
+        old_node1 = TextNode("This is a **bold** word", TextType.PLAIN)
+        old_node2 = TextNode("The last word is **bold**", TextType.PLAIN)
+        self.assertEqual(
+            split_nodes_delimiter([old_node1, old_node2], '**', TextType.BOLD),
+            [
+                TextNode("This is a ", TextType.PLAIN),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" word", TextType.PLAIN),
+                TextNode("The last word is ", TextType.PLAIN),
+                TextNode("bold", TextType.BOLD),
+                TextNode("", TextType.PLAIN)
+            ]
+        )
+    
+    def test_italic(self):
+        old_node1 = TextNode("This is an _italicized_ word", TextType.PLAIN)
+        old_node2 = TextNode("The last word is _italicized_", TextType.PLAIN)
+        self.assertEqual(
+            split_nodes_delimiter([old_node1, old_node2], '_', TextType.ITALIC),
+            [
+                TextNode("This is an ", TextType.PLAIN),
+                TextNode("italicized", TextType.ITALIC),
+                TextNode(" word", TextType.PLAIN),
+                TextNode("The last word is ", TextType.PLAIN),
+                TextNode("italicized", TextType.ITALIC),
+                TextNode("", TextType.PLAIN)
+            ]
+        )
+    
+    def test_code(self):
+        old_node1 = TextNode("This is a `code` word", TextType.PLAIN)
+        old_node2 = TextNode("The last word is `code`", TextType.PLAIN)
+        self.assertEqual(
+            split_nodes_delimiter([old_node1, old_node2], '`', TextType.CODE),
+            [
+                TextNode("This is a ", TextType.PLAIN),
+                TextNode("code", TextType.CODE),
+                TextNode(" word", TextType.PLAIN),
+                TextNode("The last word is ", TextType.PLAIN),
+                TextNode("code", TextType.CODE),
+                TextNode("", TextType.PLAIN)
+            ]
+        )
+    
+    def test_non_plain_input(self):
+        old_node1 = TextNode("This node is **already** italicized", TextType.ITALIC)
+        old_node2 = TextNode("The last word is **bold**", TextType.PLAIN)
+        self.assertEqual(
+            split_nodes_delimiter([old_node1, old_node2], '**', TextType.BOLD),
+            [
+                TextNode("This node is **already** italicized", TextType.ITALIC),
+                TextNode("The last word is ", TextType.PLAIN),
+                TextNode("bold", TextType.BOLD),
+                TextNode("", TextType.PLAIN)
+            ]
+        )
+
+    def test_invalid_markdown(self):
+        bad_node = TextNode("This is **invalid Markdown", TextType.PLAIN)
+        self.assertRaisesRegex(
+            Exception,
+            "missing closing delimiter; invalid Markdown syntax",
+            split_nodes_delimiter,
+            [bad_node], '**', TextType.BOLD
+        )
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
     def test_text(self):
